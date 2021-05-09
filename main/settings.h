9373957a43d9027c2a86205671561b83f416a868
Wouter Loeve <wouterloeve0@gmail.com>
2021-05-09 14:59:36 +0200

    Sending via Lora works

--- main/settings.h
+++ main/settings.h
@@ -1,11 +1,22 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define WIFI_SSID           CONFIG_ESP_WIFI_SSID
-#define WIFI_PASS           CONFIG_ESP_WIFI_PASSWORD
-#define WIFI_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
-#define SERVER_HTTP_PORT    CONFIG_ESP_SERVER_HTTP_PORT
-#define SERVER_ADDR         CONFIG_ESP_SERVER_ADDR
-#define DEEPSLEEP_MIN       CONFIG_ESP_DEEPSLEEP_TIME_MINUTES
+#include "lmic.h"
+
+// #define WIFI_SSID           CONFIG_ESP_WIFI_SSID
+// #define WIFI_PASS           CONFIG_ESP_WIFI_PASSWORD
+// #define WIFI_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+// #define SERVER_HTTP_PORT    CONFIG_ESP_SERVER_HTTP_PORT
+// #define SERVER_ADDR         CONFIG_ESP_SERVER_ADDR
+// #define DEEPSLEEP_MIN       CONFIG_ESP_DEEPSLEEP_TIME_MINUTES
+
+const lmic_pinmap lmic_pins = {
+  .nss = 18,
+  .rst = 14,
+  .dio = { 26, 34, 35 },
+  .spi = { /* MISO = */ 19, /* MOSI = */ 27, /* SCK = */ 5 },
+  .rxtx = LMIC_UNUSED_PIN,
+};
+
 
 #endif /* SETTINGS_H */
